ab69ff020584d750c7572c2849f5eed5d2b3bb7a
rakotoarisoandry85 <rktsoandry@gmail.com>
2026-05-19 16:25:00 +0100

    Config vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   define: {
     __APP_ENV__: JSON.stringify(process.env.VITE_VERCEL_ENV),
   },
